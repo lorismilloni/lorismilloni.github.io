ec6bc82d79f7547c0b188554fd4839babb7cf166
lorismilloni <lorismsimon@protonmail.comr>
2023-07-18 16:10:36 -0300

    feat: implement div on javascript

---
+++ box.js
@@ -0,0 +1,33 @@
+// LOAD HOME PAGE
+
+function loadPage() {
+    const box = document.getElementById('box');
+
+    fetch('home.html')
+        .then(response => response.text())
+        .then(html => {
+            box.innerHTML = html;
+        })
+        .catch(error => {
+            console.log(error);
+    });
+}
+
+loadPage();
+
+function loadPost(element) {
+    const id = element.id;
+    const link = `./posts/post-${id}.html`
+    console.log(link);
+    console.log(typeof(link));
+
+    fetch(link)
+        .then(response => response.text())
+        .then(html => {
+            box.innerHTML = html;
+        })
+        .catch(error => {
+            console.log(error);
+    });
+}
+
